05a34f9b8a17594226518b4c1d109b0106dd96eb
Smitharry <kuznetsovamaria1996@gmail.com>
2019-10-24 14:29:40 +0300

    First commit

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup
 
 setup(name='imagesr',
-      version='1.0',
-      description='Image Superresolution project',
+      version='0.1',
+      description='Image Super Resolution project',
       url="https://github.com/Smitharry/ImageSuperResolution",
       author='Maria Kuznetsova',
       author_email='kuznetsovamaria1996@gmail.com',
